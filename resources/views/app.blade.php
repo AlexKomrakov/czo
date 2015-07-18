a67cfa8831bd699653fd1672ec2f735213054f4e
Alex Komrakov <a.komrakov@smart-crowd.ru>
2015-07-18 20:01:02 +0300

    + верстка главной страницы

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,32 +7,178 @@
     <script src="{{elixir("js/app.js")}}"></script>
 
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link href='http://fonts.googleapis.com/css?family=PT+Serif:400,700,400italic,700italic&subset=cyrillic-ext,latin' rel='stylesheet' type='text/css'>
 
 </head>
 <body>
 
 <nav>
-    <div class="nav-wrapper">
-        <a href="#!" class="brand-logo">Logo</a>
+    <div class="nav-wrapper container" style="overflow: hidden;">
+        <a href="#!" class="brand-logo">
+            <img src="/public/img/logo.png" style="position: relative; height: 100%;">
+        </a>
         <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
-        <ul class="right hide-on-med-and-down">
-            <li><a href="sass.html">Sass</a></li>
-            <li><a href="badges.html">Components</a></li>
-            <li><a href="collapsible.html">Javascript</a></li>
-            <li><a href="mobile.html">Mobile</a></li>
+        <ul class="right hide-on-med-and-down" style="margin-left: 150px;">
+            <li><a class="waves-effect waves-light btn blue-grey" href="">Контакты</a></li>
+            <li><a class="waves-effect waves-light btn blue-grey" href="">Услуги</a></li>
+            <li><a class="waves-effect waves-light btn blue-grey" href="">Клиенты и отзывы</a></li>
+            <li><a class="waves-effect waves-light btn blue-grey" href="">Вопросы и ответы</a></li>
         </ul>
         <ul class="side-nav" id="mobile-demo">
-            <li><a href="sass.html">Sass</a></li>
-            <li><a href="badges.html">Components</a></li>
-            <li><a href="collapsible.html">Javascript</a></li>
-            <li><a href="mobile.html">Mobile</a></li>
+            <li><a href="">Контакты</a></li>
+            <li><a href="">Услуги</a></li>
+            <li><a href="">Клиенты и отзывы</a></li>
+            <li><a href="">Вопросы и ответы</a></li>
         </ul>
     </div>
 </nav>
 
+<div class="section no-pad-bot" id="index-banner">
+    <div class="container">
+        <div class="row">
+            <p class="header col s12 light">Фирма ООО «Центр земельных отношений» создана в … году.  Она объединила высококвалифицированных специалистов, обладающих большим опытом работы. Наличие современного геодезического оборудования позволяет качественно выполнить полный комплекс работ в интересах заказчиков.</p>
+        </div>
+    </div>
+</div>
+
+<div class="row">
+    <div class="parallax-container" style="height: auto;">
+        <div class="parallax"><img src="/public/img/0004.jpg"></div>
+        <div class="container">
+            <div class="row" style="padding: 50px 0; margin: 0;">
+                <div class="col s4">
+                    <img style="max-width: 100%;" src="/public/img/icon-1.png">
+                </div>
+                <div class="col s8 white-text">
+                    <div>
+                        Межевание земель представляет собой комплекс инженерно-геодезических работ по установлению, восстановлению и закреплению на местности границ землепользований, определению местоположения границ и площади участка, а также юридическому оформлению полученных материалов.
+                    </div>
+                    <a style="margin-top: 25px;" class="waves-effect waves-light btn green darken-4" href="">Вопросы и ответы</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="row">
+<div class="container">
+    <div class="row">
+        <div class="col s12">
+            <h5>Фирма ООО «Центр земельных отношений» предлагает широкий спектр услуг, а именно:</h5>
+        </div>
+        <div class="col s6">
+            <ul class="disc">
+                <li>Межевание земельных участков</li>
+                <li>Заказ и оформление кадастрового паспорта</li>
+                <li>Восстановление (установление) границ  земельного участка</li>
+                <li>Топографическая съемка местности</li>
+                <li>Картографические услуги</li>
+            </ul>
+        </div>
+        <div class="col s6">
+            <ul class="disc">
+                <li>Исполнительная съемка</li>
+                <li>Рыночная оценка стоимости недвижимости</li>
+                <li>Оформление земельных долей (земли с\х назначения)</li>
+                <li>Оформление объектов недвижимости</li>
+            </ul>
+        </div>
+        <div class="col s12">
+            <a style="margin-top: 25px;" class="waves-effect waves-light btn lime darken-4" href="">Вопросы и ответы</a>
+        </div>
+    </div>
+</div>
+</div>
+
+<div class="row">
+    <div class="parallax-container" style="height: auto;">
+        <div class="parallax"><img src="/public/img/0003.jpg"></div>
+        <div class="container">
+            <div class="row" style="padding: 50px 0; margin: 0;">
+                <div class="col s6 offset-s3 white-text">
+                    <div>
+                        Нашими клиентами являются компании Нижнего Новгорода и Нижегородской области.
+                        <br>
+                        <br>
+                        Фирма и ее руководство неоднократно отмечены благодарственными письмами, грамотами  от руководства района и области.                    </div>
+                    <div class="right-align">
+                        <a style="margin-top: 25px;" class="waves-effect waves-light btn blue-grey" href="">Клиенты и отзывы</a>
+                    </div>
+                </div>
+                <div class="col s3">
+                    <img style="max-width: 100%;" src="/public/img/icon-2.png">
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="row">
+<div class="container">
+    <div class="row">
+        <div class="col s12">
+            <h5>Вы хотели узнать:</h5>
+        </div>
+        <div class="col s4">
+            <h3 class="center-align">
+                <img height="100" width="100" src="/public/img/icon-question.png">
+            </h3>
+            <ul class="disc">
+                <li>Кто выполняет кадастровые работы?</li>
+                <li>Что такое технический план?</li>
+                <li>Что такое Акт обследования</li>
+            </ul>
+        </div>
+        <div class="col s6 offset-s2">
+            <h3 class="center-align">
+                <img height="100" width="100" src="/public/img/icon-question.png">
+            </h3>
+            <ul class="disc">
+                <li>Какие документы необходимо предоставить для заключения договора подряда, чтобы выполнить работы по уточнению границ и площади земельного участка</li>
+            </ul>
+        </div>
+        <div class="col s12">
+            <div class="valign-wrapper" style="margin-top: 25px;">
+                <a class="valign waves-effect waves-light btn lime darken-4" href="">Узнайте ответы</a>
+                <a style="margin-left: 25px; text-decoration: underline;" class="valign" href="">Или задайте свой вопрос</a>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
+
+<div class="col s12 blue-grey" style="padding: 25px 0;">
+    <div class="container">
+        <div class="row" style="position: relative;">
+
+            <div class="col s6">
+                <h5>ООО «Центр земельных отношений»</h5>
+                Юридический адрес: г. Бор, ул. Рослякова д. 12<br>
+                Тел. 8 (83159) 2-40-65, 9-93-00.<br>
+                Директор Шишкин Юрий Викторович<br>
+                Лицензия ВВГ-01728Г от 18 февраля 2008 г.<br>
+                Члены НП СРО «Земля и Недвижимость» г. Москва №000050<br>
+                Внесены в реестр «Кадастровые инженеры»<br>
+
+                <ul style="margin-bottom: 0; margin-top: 25px;">
+                    <li style="display: inline;"><a class="black-text btn-flat" href="">Контакты</a></li>
+                    <li style="display: inline;"><a class="black-text btn-flat" href="">Услуги</a></li>
+                    <li style="display: inline;"><a class="black-text btn-flat" href="">Клиенты и отзывы</a></li>
+                    <li style="display: inline;"><a class="black-text btn-flat" href="">Вопросы и ответы</a></li>
+                </ul>
+            </div>
+            <img class="card-title" src="/public/img/ds_logo.png" style="position: absolute; bottom: 0; right: 0;">
+
+        </div>
+    </div>
+</div>
+
 <script>
     $( document ).ready(function() {
         $(".button-collapse").sideNav();
+    });
+    $(document).ready(function(){
+        $('.parallax').parallax();
     });
 </script>
 
