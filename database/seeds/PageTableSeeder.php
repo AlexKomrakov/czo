17d57f206766588b90d8d0bb0c2ae46e539e9654
Alex Komrakov <a.komrakov@smart-crowd.ru>
2015-07-23 15:47:54 +0300

    + faq

--- database/seeds/PageTableSeeder.php
+++ database/seeds/PageTableSeeder.php
@@ -17,20 +17,24 @@ class PageTableSeeder extends Seeder
             [
                 'id' => 'contacts',
                 'title' => 'Контакты',
+                'description' => '',
                 'body' => file_get_contents(base_path() . '/database/seeds/pages/contacts.blade.php'),
             ],
             [
                 'id' => 'clients',
                 'title' => 'Клиенты и отзывы',
+                'description' => '',
             ],
             [
                 'id' => 'uslugi',
                 'title' => 'Услуги',
+                'description' => '',
                 'body' => file_get_contents(base_path() . '/database/seeds/pages/uslugi.blade.php'),
             ],
             [
                 'id' => 'faq',
                 'title' => 'Вопросы и ответы',
+                'description' => 'Какова стоимость кадастровых работ. Какие документы необходимо предоставить для заключения договора подряда. Межевание. Другие вопросы.',
                 'body' => file_get_contents(base_path() . '/database/seeds/pages/faq.blade.php'),
             ],
         ];
