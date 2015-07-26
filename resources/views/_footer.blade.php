ba33b27f87c1f1fb4e96c57ee0140ef6ece1050d
Alex Komrakov <a.komrakov@smart-crowd.ru>
2015-07-26 14:47:26 +0300

    + changed footer menu

--- resources/views/_footer.blade.php
+++ resources/views/_footer.blade.php
@@ -1,6 +1,14 @@
 <footer class="page-footer blue-grey">
     <div class="container" style="padding-bottom: 25px;">
         <div class="row pos-relative m-none">
+            <div class="col s12">
+                <ul style="margin-bottom: 25px; margin-left: -20px;">
+                    <li class="display-inline"><a class="black-text btn blue-grey {{Request::is('uslugi') ? "darken-2 white-text" : ""}}" href="/uslugi">Услуги</a></li>
+                    <li class="display-inline"><a class="black-text btn blue-grey {{Request::is('faq') ? "darken-2 white-text" : ""}}" href="/faq">Вопросы и ответы</a></li>
+                    <li class="display-inline"><a class="black-text btn blue-grey {{Request::is('clients') ? "darken-2 white-text" : ""}}" href="/clients">Клиенты и отзывы</a></li>
+                    <li class="display-inline"><a class="black-text btn blue-grey {{Request::is('contacts') ? "darken-2 white-text" : ""}}" href="/contacts">Контакты</a></li>
+                </ul>
+            </div>
             <div class="col s12 m7">
                 <h6>ООО «Центр земельных отношений»</h6>
                 <small>
@@ -12,12 +20,6 @@
                     Члены НП СРО «Земля и Недвижимость» г. Москва №000050<br>
                     Внесены в реестр «Кадастровые инженеры»<br>
                 </small>
-                <ul style="margin-bottom: 0; margin-top: 25px; margin-left: -20px;">
-                    <li class="display-inline"><a class="black-text btn blue-grey {{Request::is('uslugi') ? "darken-2 white-text" : ""}}" href="/uslugi">Услуги</a></li>
-                    <li class="display-inline"><a class="black-text btn blue-grey {{Request::is('faq') ? "darken-2 white-text" : ""}}" href="/faq">Вопросы и ответы</a></li>
-                    <li class="display-inline"><a class="black-text btn blue-grey {{Request::is('clients') ? "darken-2 white-text" : ""}}" href="/clients">Клиенты и отзывы</a></li>
-                    <li class="display-inline"><a class="black-text btn blue-grey {{Request::is('contacts') ? "darken-2 white-text" : ""}}" href="/contacts">Контакты</a></li>
-                </ul>
             </div>
             <div class="col s12 m5" style="min-height: 100px;">
                 <a href="http://www.ooods.ru" target="_blank">
