e7c9c954474cf528f039cd05f01f79d7acc8121d
Alex Komrakov <a.komrakov@smart-crowd.ru>
2015-07-27 17:57:45 +0300

    + lightbox

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -59,7 +59,8 @@
 
     lightbox.option({
         'resizeDuration': 200,
-        'showImageNumberLabel': false
+        'showImageNumberLabel': false,
+        'wrapAround': true
     })
 </script>
 
