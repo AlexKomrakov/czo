6b884abbaa8367ef4c7826307632f842c81dbdae
Alex Komrakov <a.komrakov@smart-crowd.ru>
2015-08-05 22:27:54 +0300

    + some changes

--- database/seeds/pages/clients.blade.php
+++ database/seeds/pages/clients.blade.php
@@ -7,27 +7,27 @@
         <div class="col s12" style="margin-bottom: 25px;">
             <h5>Награды и благодарности</h5>
         </div>
-        <div class="col s6 m6 l2 offset-l1 center-align">
+        <div class="col s6 m6 l3 center-align">
             <a href="/public/img/diploms/dip01.jpg" data-lightbox="diploms">
                 <img class="responsive-img" src="/public/img/diploms/dip01.jpg">
             </a>
         </div>
-        <div class="col s6 m6 l2 center-align">
+        <div class="col s6 m6 l3 center-align">
             <a href="/public/img/diploms/dip02.jpg" data-lightbox="diploms">
                 <img class="responsive-img" src="/public/img/diploms/dip02.jpg">
             </a>
         </div>
-        <div class="col s6 m6 l2 center-align">
+        <div class="col s6 m6 l3 center-align">
             <a href="/public/img/diploms/dip03.jpg" data-lightbox="diploms">
                 <img class="responsive-img" src="/public/img/diploms/dip03.jpg">
             </a>
         </div>
-        <div class="col s6 m6 l2 center-align">
+        <div class="col s6 m6 l3 center-align">
             <a href="/public/img/diploms/dip04.jpg" data-lightbox="diploms">
                 <img class="responsive-img" src="/public/img/diploms/dip04.jpg">
             </a>
         </div>
-        <div class="col s6 m6 l2 center-align">
+        <div class="col s6 m6 l3 center-align">
             <a href="/public/img/diploms/dip05.jpg" data-lightbox="diploms">
                 <img class="responsive-img" src="/public/img/diploms/dip05.jpg">
             </a>
