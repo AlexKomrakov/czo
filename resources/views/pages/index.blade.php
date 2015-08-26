7eebc7760d43a73d1d445c75026fd50666de074c
Alex Komrakov <a.komrakov@smart-crowd.ru>
2015-08-27 00:30:56 +0300

    + ключевые слова в таблице страниц

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -8,23 +8,25 @@
         <table>
             <thead>
             <tr>
-                <th data-field="id">Имя</th>
-                <th data-field="name">Описание</th>
-                <th data-field="name">Управление</th>
+                <th>Имя</th>
+                <th>Описание</th>
+                <th>Ключевые слова</th>
+                <th>Управление</th>
             </tr>
             </thead>
 
             <tbody>
             @foreach($pages as $page)
             <tr>
-                <td>
+                <td width="30">
                     {{$page->title}}
                     <div>
                         <small>{{$page->id}}</small>
                     </div>
                 </td>
-                <td>{{$page->description}}</td>
-                <td>
+                <td width="30"><small>{{$page->description}}</small></td>
+                <td width="10"><small>{{$page->keywords}}</small></td>
+                <td width="20">
                     <a class="btn red" href="/admin/pages/{{$page->id}}/delete">Удалить</a>
                     <a class="btn" href="/admin/pages/{{$page->id}}/edit">Изменить</a>
                 </td>
