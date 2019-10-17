910a3ffa56bd005d2d29f4987ccc626ea00f301b
Alex <alexkomrakov@gmail.com>
2019-10-17 23:50:40 +0300

    Update clients.blade.php

--- database/seeds/pages/clients.blade.php
+++ database/seeds/pages/clients.blade.php
@@ -8,27 +8,27 @@
             <h5>Награды и благодарности</h5>
         </div>
         <div class="col s6 m6 l3 center-align">
-            <a href="/public/img/diploms/dip01.jpg" data-lightbox="diploms">
+            <a href="/img/diploms/dip01.jpg" data-lightbox="diploms">
                 <img class="responsive-img" src="/public/img/diploms/dip01.jpg">
             </a>
         </div>
         <div class="col s6 m6 l3 center-align">
-            <a href="/public/img/diploms/dip02.jpg" data-lightbox="diploms">
+            <a href="/img/diploms/dip02.jpg" data-lightbox="diploms">
                 <img class="responsive-img" src="/public/img/diploms/dip02.jpg">
             </a>
         </div>
         <div class="col s6 m6 l3 center-align">
-            <a href="/public/img/diploms/dip03.jpg" data-lightbox="diploms">
+            <a href="/img/diploms/dip03.jpg" data-lightbox="diploms">
                 <img class="responsive-img" src="/public/img/diploms/dip03.jpg">
             </a>
         </div>
         <div class="col s6 m6 l3 center-align">
-            <a href="/public/img/diploms/dip04.jpg" data-lightbox="diploms">
+            <a href="/img/diploms/dip04.jpg" data-lightbox="diploms">
                 <img class="responsive-img" src="/public/img/diploms/dip04.jpg">
             </a>
         </div>
         <div class="col s6 m6 l3 center-align">
-            <a href="/public/img/diploms/dip05.jpg" data-lightbox="diploms">
+            <a href="/img/diploms/dip05.jpg" data-lightbox="diploms">
                 <img class="responsive-img" src="/public/img/diploms/dip05.jpg">
             </a>
         </div>
